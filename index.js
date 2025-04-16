f6c96b57e32c11ff58baab3bf5ec933631aa19a8
gloriacostaa <gloria.costaaraujo25@gmail.com>
2025-04-16 20:16:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+let display = document.getElementById("display");
 
+function limpar() {
+    display.value = '';
+}
+
+function adicionarValor(valor) {
+    display.value += valor;
+}
+
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Erro';
+    }
+}
